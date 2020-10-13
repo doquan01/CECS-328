0b4eae758790261524a961aeac540e9f3bf9e4e8
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-12 19:52:20 -0700

    Update 10/12/2020

--- Program 3/src/Main.java
+++ Program 3/src/Main.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         File fl = new File("input.txt");
         Scanner scan = new Scanner(fl);
         int totMag = scan.nextInt();
-        String y = scan.next();
         String x = scan.next();
+        String y = scan.next();
         char[][] inputMag = new char[y.length()][x.length()];
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < y.length(); j++){
@@ -25,14 +24,16 @@ public class Main {
             }
         }
 
+
         ArrayList<Integer> existing = new ArrayList<>();
-        for(int i = 0; i < y.length(); i++){
-            if((inputMag[0][i] == '+' && inputMag[1][i] == '-') || (inputMag[0][i] == '-' && inputMag[1][i] == '+')){
-                existing.add(i);
-            }
+        checkExisting(y, inputMag, existing);
+        if(!existing.isEmpty()){
+            existingfill(y, inputMag, existing);
         }
-        int count = 0;
-        
+        else{
+            addNoEx(y, inputMag, existing);
+        }
+
         int[][] coord = new int[totMag][4];
 
 
@@ -46,8 +47,165 @@ public class Main {
             System.out.println(existing.get(i));
         }
 
+
         FileWriter writer = new FileWriter("output.txt");
 //        writer.write();
         writer.close();
     }
+    public static void checkExisting(String y, char[][] c, ArrayList<Integer> e){
+        for(int i = 0; i < y.length(); i++){
+            if((c[0][i] == '+' && c[1][i] == '-') || (c[0][i] == '-' && c[1][i] == '+')){
+                e.add(i);
+            }
+        }
+    }
+//    public static void existingfill(String y, char[][] c, ArrayList<Integer> e){
+//        if(e.get(0) == 0){
+//            if(c[0][0] == '+'){
+//                for(int i = 0; i < y.length(); i++){
+//                    if(i % 2 == 0){
+//                        c[0][i] = '+';
+//                        c[1][i] = '-';
+//                    }
+//                    else{
+//                        c[0][i] = '-';
+//                        c[1][i] = '+';
+//                    }
+//                }
+//            }
+//            else if(c[0][0] == '-'){
+//                for(int i = 0; i < y.length(); i++){
+//                    if(i % 2 == 0){
+//                        c[0][i] = '-';
+//                        c[1][i] = '+';
+//                    }
+//                    else{
+//                        c[0][i] = '+';
+//                        c[1][i] = '-';
+//                    }
+//                }
+//            }
+//        }
+//        else{
+//            if(c[0][e.get(0)] == '+'){
+//                for(int i = e.get(0); i < y.length(); i++){
+////                    if(e.get(0) % 2 == 0) {
+////                        if (i % 2 == 0) {
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        } else {
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                    }
+////                    else{
+////                        if (i % 2 != 0) {
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        } else {
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                    }
+////                }
+////                for(int i = e.get(0); i >= 0; i--){
+////                    if(e.get(0) % 2 == 0){
+////                        if(i % 2 == 0){
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                        else{
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                    }
+////                    else{
+////                        if(i % 2 != 0){
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                        else{
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                    }
+//                }
+//            }
+////            if(c[0][e.get(0)] == '-'){
+////                for(int i = e.get(0); i < y.length(); i++){
+////                    if(e.get(0) % 2 == 0){
+////                        if(i % 2 == 0){
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                        else{
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                    }
+////                    else{
+////                        if(i % 2 != 0){
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                        else{
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                    }
+////                }
+////                for(int i = e.get(0); i >= 0; i--){
+////                    if(e.get(0) % 2 == 0){
+////                        if(i % 2 == 0){
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                        else{
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                    }
+////                    else{
+////                        if(i % 2 != 0){
+////                            c[0][i] = '-';
+////                            c[1][i] = '+';
+////                        }
+////                        else{
+////                            c[0][i] = '+';
+////                            c[1][i] = '-';
+////                        }
+////                    }
+////                }
+////            }
+//        }
+//    }
+    public static void addNoEx(String y, char[][] c, ArrayList<Integer> t){
+        for(int i = 0; i < y.length(); i++){
+            if(c[0][i] != '*' || c[1][i] != '*'){
+                t.add(i);
+            }
+        }
+        if(!t.isEmpty()){
+            if(c[0][t.get(0)] == '+'){
+                c[1][t.get(0)] = '-';
+            }
+            else if(c[0][t.get(0)] == '-'){
+                c[1][t.get(0)] = '+';
+            }
+            else if(c[1][t.get(0)] == '+'){
+                c[0][t.get(0)] = '-';
+            }
+            else if(c[1][t.get(0)] == '-'){
+                c[0][t.get(0)] = '+';
+            }
+        }
+        else{
+            c[0][0] = '+';
+            c[1][0] = '-';
+            t.add(0);
+        }
+//        existingfill(y, c, t);
+        t.clear();
+    }
+
 }
