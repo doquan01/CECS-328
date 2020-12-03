0266e6b836e8e63fd503ab880c09cfe65c43573f
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-12-02 20:15:01 -0800

    update 12/2/2020

--- Program 5/src/Main.java
+++ Program 5/src/Main.java
@@ -1,3 +1,4 @@
+//Quan Do
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,64 +11,195 @@ public class Main {
         File fl = new File("input.txt");
         Scanner scan = new Scanner(fl);
 
-        ArrayList<Pad> path = new ArrayList<>();
+        ArrayList<Pad> pads = new ArrayList<>();
         while(scan.hasNextLine()){
-            path.add(new Pad(scan.nextLine()));
+            pads.add(new Pad(scan.nextLine()));
         }
 
-        checkMinMax(path);
+        checkMin(pads);
+        checkMax(pads);
 
-        for(int i = 0; i < path.size(); i++){
-            System.out.println(path.get(i).toString());
+//        for(int i = 0; i < pads.size(); i++){
+//            System.out.println(pads.get(i).toString());
+//        }
+
+        FileWriter writer = new FileWriter("output.txt");
+        ArrayList<Pad> path = new ArrayList<>();
+        int loop = 0;
+        Pad start = new Pad("1");
+        Pad curr = start;
+        path.add(curr);
+        for(int i = 0; i < pads.size(); i++){
+            if(curr == start && pads.get(i).isMinimal() && !pads.get(i).isMaximal() && !pads.get(i).isDropped() && pads.get(i).isUseful()){
+                path.add(pads.get(i));
+                curr = pads.get(i);
+                loop = 0;
+//                System.out.println("1");
+            }
+            else if(curr != start && curr.getValue().compareTo(pads.get(i).getValue()) == -1 && !curr.getValue().gcd(pads.get(i).getValue()).equals(BigInteger.ONE) && !pads.get(i).isDropped() && pads.get(i).isUseful()){
+                if(pads.get(i).isMaximal() && !pads.get(i).isMinimal()){
+                    path.add(pads.get(i));
+                    for(int j = 0; j < path.size(); j++){
+                        path.get(j).setDropped();
+                        writer.write(path.get(j).getValue() + " ");
+                        pads.remove(path.get(j));
+                    }
+                    writer.write("\n");
+                    path = new ArrayList<>();
+                    curr = start;
+                    path.add(curr);
+                    i = 0;
+                    loop = 0;
+//                    System.out.println("3");
+                }
+            }
+            else if(curr == start && pads.get(i).isMinimal() && pads.get(i).isMaximal() && !pads.get(i).isDropped() && pads.get(i).isUseful()){
+                path.add(pads.get(i));
+                for(int j = 0; j < path.size(); j++){
+                    path.get(j).setDropped();
+                    pads.remove(path.get(j));
+                    writer.write(path.get(j).getValue() + " ");
+                }
+                writer.write("\n");
+                path = new ArrayList<>();
+                curr = start;
+                path.add(curr);
+                i = 0;
+                loop = 0;
+//                System.out.println("4");
+            }
+            else if(curr.isMinimal() && i == pads.size()-1 && (pads.get(pads.size()-1).getValue().gcd(curr.getValue()).equals(BigInteger.ONE))){
+                if(loop == 0){
+                    loop++;
+                    i = 0;
+                }
+                else if(loop != 0){
+                    curr.setUsefulFalse();
+                    path = new ArrayList<>();
+                    curr = start;
+                    path.add(curr);
+                    i = 0;
+                    loop = 0;
+                }
+            }
+        }
+        for(int i = 0; i < pads.size(); i++){
+            pads.get(i).setUseful();
         }
 
-//        FileWriter writer = new FileWriter("output.txt");
-////        writer.write();
-//        writer.close();
+        path = new ArrayList<>();
+        loop = 0;
+        curr = start;
+        path.add(curr);
+        for(int i = 0; i < pads.size(); i++){
+            if(curr == start && pads.get(i).isMinimal() && !pads.get(i).isMaximal() && !pads.get(i).isDropped() && pads.get(i).isUseful()){
+                path.add(pads.get(i));
+                curr = pads.get(i);
+                loop = 0;
+//                System.out.println("1");
+            }
+            else if(!(pads.get(i).getValue().gcd(curr.getValue()).equals(BigInteger.ONE)) && curr.getValue().compareTo(pads.get(i).getValue()) == -1 && pads.get(i).isUseful()){
+                if(!pads.get(i).isMaximal()){
+                    path.add(pads.get(i));
+                    curr = pads.get(i);
+                    loop = 0;
+                }
+                else{
+                    path.add(pads.get(i));
+                    for(int j = 0; j < path.size(); j++){
+                        path.get(j).setDropped();
+                        writer.write(path.get(j).getValue() + " ");
+                        pads.remove(path.get(j));
+                    }
+                    writer.write("\n");
+                    path = new ArrayList<>();
+                    curr = start;
+                    path.add(curr);
+                    i = 0;
+                    loop = 0;
+                }
+            }
+            else if((curr.isMinimal() || !curr.isMinimal()) && !curr.isMaximal() && i == pads.size()-1){
+                if(loop == 0){
+                    loop++;
+                    i = 0;
+                }
+                else if(loop != 0){
+                    path.get(1).setUsefulFalse();
+                    path = new ArrayList<>();
+                    curr = start;
+                    path.add(curr);
+                    i = 0;
+                    loop = 0;
+                }
+            }
+        }
+        for(int i = 0; i < pads.size(); i++){
+            System.out.println(pads.get(i).toString());
+        }
+//        for(int i = 0; i < pads.size(); i++){
+//            if(curr == start && pads.get(i).isMinimal() && !pads.get(i).isMaximal() && !pads.get(i).isDropped()){
+//                path.add(pads.get(i));
+//                curr = pads.get(i);
+//            }
+//        }
+//        System.out.println(curr);
+//        for(int i = 0; i < pads.size(); i++){
+//            if(!(pads.get(i).getValue().gcd(curr.getValue()).equals(BigInteger.ONE)) && pads.get(i).getValue().compareTo(curr.getValue()) == 1){
+//                for(int j = 0; j < pads.size(); j++) {
+//                    if(!(pads.get(i).getValue().gcd(pads.get(j).getValue()).equals(BigInteger.ONE)) && pads.get(j).isMaximal()){
+//                        path.add(pads.get(i));
+//                        path.add(pads.get(j));
+//                        writer.write("\n1" + " " + curr.getValue() + " " + pads.get(i).getValue() + " " + pads.get(j).getValue() + " ");
+//                        pads.remove(pads.get(i));
+//                        pads.remove(pads.get(j));
+//                        curr.setDropped();
+//                        curr = start;
+//                        pads.remove(curr);
+//                        for(int k = 0; k < pads.size(); k++){
+//                            if(curr == start && pads.get(i).isMinimal() && !pads.get(i).isMaximal() && !pads.get(i).isDropped()){
+//                                path.add(pads.get(i));
+//                                curr = pads.get(i);
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+//        writer.write();
+        writer.close();
     }
-    public static void checkMinMax(ArrayList<Pad> p){
-        int factor = 0;
-        int genFactor = 0;
+    public static void checkMin(ArrayList<Pad> p){
         for(int i = 0; i < p.size(); i++){
+            p.get(i).setMinimal();
             for(int j = 0; j < p.size(); j++){
                 if(i == j){
                     continue;
                 }
                 else{
-                    if(!p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE)){
-                        genFactor++;
-                    }
-                    if(p.get(i).getValue().isProbablePrime(1)){
-                        p.get(i).setMinimal();
-                    }
-                    if(p.get(i).getValue().compareTo(p.get(j).getValue()) == -1 && !(p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))) {
-                        p.get(i).setMinimal();
-                    }
-                    if(p.get(i).isMaximal() && genFactor != 0){
-                    p.get(i).setMinimalFalse();
-                    }
-
-                    if(p.get(i).getValue().compareTo(p.get(j).getValue()) == -1 && !(p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))){
-                        factor++;
-                    }
-                    if(factor == 0){
-                        if(p.get(i).getValue().compareTo(p.get(j).getValue()) == 1 && !(p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))){
-                            p.get(i).setMaximal();
-                        }
-                        if(p.get(i).getValue().compareTo(p.get(j).getValue()) == 1 && p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE)){
-                            p.get(i).setMaximal();
-                        }
-                        if(p.get(i).getValue().compareTo(p.get(j).getValue()) == 1 && (p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))){
-                            p.get(i).setMinimal();
-                        }
-                    }
-                    if(factor != 0){
-                        p.get(i).setMaximalFalse();
+                    if(p.get(i).getValue().compareTo(p.get(j).getValue()) == 1 && !(p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))){
+                        p.get(i).setMinimalFalse();
+                        break;
                     }
                 }
             }
-            factor = 0;
-            genFactor = 0;
+        }
+    }
+    public static void checkMax(ArrayList<Pad> p){
+        for(int i = 0; i < p.size(); i++){
+            p.get(i).setMaximal();
+            for(int j = 0; j < p.size(); j++){
+                if(i == j){
+                    continue;
+                }
+                else{
+                    if(p.get(i).getValue().compareTo(p.get(j).getValue()) == -1 && !(p.get(i).getValue().gcd(p.get(j).getValue()).equals(BigInteger.ONE))){
+                        p.get(i).setMaximalFalse();
+                        break;
+                    }
+                }
+            }
         }
     }
     static class Pad{
@@ -75,12 +207,16 @@ public class Main {
         private boolean dropped;
         private boolean minimal;
         private boolean maximal;
+        private boolean useful;
 
         public Pad(String v){
             value = new BigInteger(v);
-            dropped = false;
+            if(value.equals(BigInteger.ONE)){
+                dropped = false;
+            }
             minimal = false;
             maximal = false;
+            useful = true;
         }
         public BigInteger getValue(){
             return value;
@@ -94,6 +230,7 @@ public class Main {
         public boolean isMaximal(){
             return maximal;
         }
+        public boolean isUseful() { return useful; }
         public void setDropped(){
             dropped = true;
         }
@@ -103,6 +240,8 @@ public class Main {
         public void setMaximal(){
             maximal = true;
         }
+        public void setUseful() { useful = true; }
+        public void setUsefulFalse() { useful = false; }
         public void setMinimalFalse(){ minimal = false; }
         public void setMaximalFalse(){
             maximal = false;
