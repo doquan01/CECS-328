b4d7302384f60b3ba7fc03c0a89e848b17eceaca
doquan01 <55120808+doquan01@users.noreply.github.com>
2021-01-19 09:59:27 -0800

    removed unnecessary comments

--- Hobbits/src/Main.java
+++ Hobbits/src/Main.java
@@ -54,80 +54,6 @@ public class Main {
                 pads.remove(pads.get(i));
             }
         }
-//        ArrayList<Pad> path = new ArrayList<>();
-//        int loop = 0;
-//        Pad start = new Pad("1");
-//        Pad curr = start;
-//        Pad temp = start;
-//        path.add(curr);
-//        for(int i = 0; i < pads.size(); i++){
-//            if(curr == start && pads.get(i).isMinimal() && !pads.get(i).isMaximal() && pads.get(i).isUseful()){
-//                path.add(pads.get(i));
-//                curr = pads.get(i);
-//                loop = 0;
-//                if(i == pads.size()-1){
-//                    if(loop == 0){
-//                        loop++;
-//                        i = 0;
-//                    }
-//                    else if(loop != 0){
-//                        temp.setUsefulFalse();
-//                        temp = start;
-//                        path = new ArrayList<>();
-//                        curr = start;
-//                        path.add(curr);
-//                        i = 0;
-//                        loop = 0;
-//                    }
-//                }
-//            }
-//            else if(curr != start && curr.getValue().compareTo(pads.get(i).getValue()) == -1 && !curr.getValue().gcd(pads.get(i).getValue()).equals(BigInteger.ONE) && !pads.get(i).isMinimal() && pads.get(i).isUseful()){
-//                if(pads.get(i).isMaximal() && !pads.get(i).isMinimal()){
-//                    path.add(pads.get(i));
-//                    for(int j = 0; j < path.size(); j++){
-//                        writer.write(path.get(j).getValue() + " ");
-//                        pads.remove(path.get(j));
-//                    }
-//                    writer.write("\n");
-//                    path = new ArrayList<>();
-//                    curr = start;
-//                    path.add(curr);
-//                    i = 0;
-//                    loop = 0;
-//                }
-//            }
-//            else if(curr == start && pads.get(i).isMinimal() && pads.get(i).isMaximal() && pads.get(i).isUseful()){
-//                path.add(pads.get(i));
-//                for(int j = 0; j < path.size(); j++){
-//                    pads.remove(path.get(j));
-//                    writer.write(path.get(j).getValue() + " ");
-//                }
-//                writer.write("\n");
-//                path = new ArrayList<>();
-//                curr = start;
-//                path.add(curr);
-//                i = 0;
-//                loop = 0;
-//            }
-//            else if(curr.isMinimal() && i == pads.size()-1 && (pads.get(pads.size()-1).getValue().gcd(curr.getValue()).equals(BigInteger.ONE))){
-//                if(loop == 0){
-//                    loop++;
-//                    i = 0;
-//                }
-//                else if(loop != 0){
-//                    curr.setUsefulFalse();
-//                    path = new ArrayList<>();
-//                    curr = start;
-//                    path.add(curr);
-//                    i = 0;
-//                    loop = 0;
-//                }
-//            }
-//        }
-//        for(int i = 0; i < pads.size(); i++){
-//            pads.get(i).setUseful();
-//        }
-//
         path = new ArrayList<>();
         curr = start;
         path.add(curr);
@@ -136,9 +62,9 @@ public class Main {
                 curr = pads.get(i);
                 path.add(pads.get(i));
                 for(int j = 0; j < pads.size(); j++){
-                     if(curr.isMinimal() && !(pads.get(j).getValue().gcd(curr.getValue()).equals(BigInteger.ONE)) && pads.get(j).getValue().compareTo(curr.getValue()) == 1 && !curr.equals(pads.get(j))){
-                         for(int k = 0; k < pads.size(); k++){
-                             if(curr.isMinimal() && !(pads.get(j).getValue().gcd(pads.get(k).getValue()).equals(BigInteger.ONE)) && pads.get(k).isMaximal() && !curr.equals(pads.get(j)) && !curr.equals(pads.get(k)) && !pads.get(j).equals(pads.get(k))){
+                    if(curr.isMinimal() && !(pads.get(j).getValue().gcd(curr.getValue()).equals(BigInteger.ONE)) && pads.get(j).getValue().compareTo(curr.getValue()) == 1 && !curr.equals(pads.get(j))){
+                        for(int k = 0; k < pads.size(); k++){
+                            if(curr.isMinimal() && !(pads.get(j).getValue().gcd(pads.get(k).getValue()).equals(BigInteger.ONE)) && pads.get(k).isMaximal() && !curr.equals(pads.get(j)) && !curr.equals(pads.get(k)) && !pads.get(j).equals(pads.get(k))){
                                 path.add(pads.get(j));
                                 path.add(pads.get(k));
                                 for(int l = 0; l < path.size(); l++){
@@ -157,22 +83,6 @@ public class Main {
                 }
             }
         }
-//        ArrayList<Pad> min = new ArrayList<>();
-//        ArrayList<Pad> max = new ArrayList<>();
-//        ArrayList<Pad> minFactor = new ArrayList<>();
-//        ArrayList<Pad> shared = new ArrayList<>();
-//        for(int i = 0; i < pads.size(); i++){
-//            if(pads.get(i).isMinimal()){
-//                min.add(pads.get(i));
-//            }
-//            else if(pads.get(i).isMaximal()){
-//                max.add(pads.get(i));
-//            }
-//        }
-//
-//        for(int i = 0; i < pads.size(); i++){
-//            System.out.println(pads.get(i).toString());
-//        }
         writer.close();
     }
     public static void checkMinMax(ArrayList<Pad> p){
@@ -209,13 +119,11 @@ public class Main {
         private BigInteger value;
         private boolean minimal;
         private boolean maximal;
-//        private boolean useful;
 
         public Pad(String v){
             value = new BigInteger(v);
             minimal = false;
             maximal = false;
-//            useful = true;
         }
         public BigInteger getValue(){
             return value;
@@ -226,21 +134,15 @@ public class Main {
         public boolean isMaximal(){
             return maximal;
         }
-//        public boolean isUseful() { return useful; }
         public void setMinimal(){
             minimal = true;
         }
         public void setMaximal(){
             maximal = true;
         }
-//        public void setUseful() { useful = true; }
-//        public void setUsefulFalse() { useful = false; }
         public void setMinimalFalse(){ minimal = false; }
         public void setMaximalFalse(){
             maximal = false;
         }
-//        public String toString(){
-//            return value + " minimal: " + minimal + " maximal: " + maximal;
-//        }
     }
 }
